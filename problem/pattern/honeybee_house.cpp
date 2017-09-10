af57ad65eaf3fb7d47d1f36051192eecac02b24a
EpicMovie <superpowerex@naver.com>
2017-09-11 00:39:52 +0900

    Create honeybee_house.cpp

---
+++ problem/pattern/honeybee_house.cpp
@@ -0,0 +1,3 @@
+#include <iostream>
+using namespace std;
+int main(){    int n;    int min = 1, max = 1;    int factor = 0;    int result = 1;        cin >> n;        while(true){        if(n >= min && n <= max){            cout << result << endl;            break;        } else {            factor += 6;            min = max + 1;            max += factor;            result++;        }    }        return 0;}
