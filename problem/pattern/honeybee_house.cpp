0852a13c2e989ce6838fe5ac9f14bc7a3add4f35
EpicMovie <superpowerex@naver.com>
2017-09-11 00:41:06 +0900

    Update honeybee_house.cpp

--- problem/pattern/honeybee_house.cpp
+++ problem/pattern/honeybee_house.cpp
@@ -1,3 +1,24 @@
 #include <iostream>
 using namespace std;
-int main(){    int n;    int min = 1, max = 1;    int factor = 0;    int result = 1;        cin >> n;        while(true){        if(n >= min && n <= max){            cout << result << endl;            break;        } else {            factor += 6;            min = max + 1;            max += factor;            result++;        }    }        return 0;}
+int main(){
+    int n;
+    int min = 1, max = 1;
+    int factor = 0;
+    int result = 1;
+    
+    cin >> n;
+    
+    while(true){
+        if(n >= min && n <= max){
+            cout << result << endl;
+            break;
+        } else {
+            factor += 6;
+            min = max + 1;
+            max += factor;
+            result++;
+        }
+    }
+    
+    return 0;
+}
